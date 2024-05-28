a03f6234b99b7bfa0363c62962100d8d459c110b
jbhcgeek2 <joelbh92@gmail.com>
2024-05-27 18:47:53 -0700

    hoila

--- assets/js/altaTrabajo.js
+++ assets/js/altaTrabajo.js
@@ -1,21 +1,6 @@
 
 document.addEventListener("DOMContentLoaded", function() {
-  Swal.fire({
-    title: 'Antes de continuar...',
-    text: 'Ya se encuentra registrado el cliente?',
-    icon: 'warning',
-    showDenyButton: true,
-    confirmButtonText: 'Ya esta registrado',
-    denyButtonText: 'No, Registrar nuevo cliente'
-  }).then((result)=>{
-    if(result.isConfirmed){
-      //ya esta registrado
-    }else{
-      //no esta registrado, lo damos de alta
-      window.location = 'altaCliente.php';
-    }
-  })
-
+  
   let tipoServ = document.getElementById('tipoServicio');
   tipoServ.addEventListener('change', function(){
     //buscaremos el precio sugerido del servicio
